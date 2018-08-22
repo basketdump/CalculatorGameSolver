8414bf3b1cba760a9edda85fd1da98e9843bd4ae
basketdump <tylerisop@gmail.com>
2018-08-22 09:44:47 -0500

    Updated some issues around float with reverse, insert

--- solver.py
+++ solver.py
@@ -1,28 +1,49 @@
 def reverse(o1):
     '''This function reverses an integer and returns it'''
     result = ''
-    if o1 == 0:
-        return 0
+    s_o1 = str(o1)
+    negative = False
+    if (o1 < 0):
+        negative = True
+        o1 = o1 * -1
+    if (o1.is_integer()):
+        o1 = int(o1)
+        if o1 == 0:
+            return 0.0
+        else:
+            while o1 > 0:
+                result += str(o1 % 10)
+                o1 = o1 // 10
+            # print(result)
     else:
-        while o1 > 0:
-            result += str(o1 % 10)
-            o1 = o1 // 10
-        return int(result)
+        for i in range(len(s_o1)):
+            result = result + s_o1[len(s_o1) - (i + 1)]
+        # print(result)
+    result = float(result)
+    if (negative):
+        return result * -1
+    else:
+        return result
 
 def test_path(f, x):
     '''This function interprets and evaluates an operation'''
-    y = x
+    y = float(x)
+    print(y)
     for i in range(len(f)):
         if o[f[i]][0] == '+':
-            y = y + int(o[f[i]][1])
+            y = y + float(o[f[i]][1])
         elif o[f[i]][0] == '*':
-            y = y * int(o[f[i]][1])
+            y = y * float(o[f[i]][1])
         elif o[f[i]][0] == '-':
-            y = y - int(o[f[i]][1])
+            y = y - float(o[f[i]][1])
         elif o[f[i]][0] == '/':
-            y = y / int(o[f[i]][1])
+            y = y / float(o[f[i]][1])
         elif o[f[i]][0] == 'i':
-            y = int(str(y) + o[f[i]][1])
+            print(y)
+            if y.is_integer():
+                y = float(str(int(y)) + o[f[i]][1])
+            else:
+                y = float(str(y) + o[f[i]][1])
         elif o[f[i]][0] == 'f':
             y = y * -1
         elif o[f[i]][0] == 'r':
@@ -32,8 +53,8 @@ def test_path(f, x):
 
 # Setup variables
 m = int(input("# of Moves: "))
-g = int(input("Goal: "))
-x = int(input("Starting Number: "))
+g = float(input("Goal: "))
+x = float(input("Starting Number: "))
 n = int(input("# of Operators: "))
 # o = [op1, op2, op3, op4]
 o = []
@@ -72,6 +93,7 @@ while current[0] < n:
 print(print(len(p), 'paths generated total'))
 
 for i in range(len(p)):
-    if test_path(p[i], x) == g:
+    print(p[i], end=' - ')
+    if test_path(p[i], float(x)) == g:
         print(p[i], 'is the correct path')
         break
