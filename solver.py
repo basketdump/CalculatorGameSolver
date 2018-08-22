f3452b955d6caecdc71e826fc1551e0a07b786be
basketdump <tylerisop@gmail.com>
2018-08-21 23:52:12 -0500

    Broken but commented

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 def reverse(o1):
+    '''This function reverses an integer and returns it'''
     result = ''
     while o1 > 0:
         result += o1 // 10
@@ -6,6 +7,7 @@ def reverse(o1):
     return int(result)
 
 def test_path(f, x):
+    '''This function interprets and evaluates an operation'''
     y = x
     for i in range(len(f)):
         if o[f[i]][0] == '+':
@@ -16,6 +18,8 @@ def test_path(f, x):
             y = y - int(o[f[i]][1])
         elif o[f[i]][0] == '/':
             y = y / int(o[f[i]][1])
+        elif o[f[i]][0] == 'i':
+            y = int(str(y) + o[f[i]][1])
         elif o[f[i]][0] == 'f':
             y = y * -1
         elif o[f[i]][0] == 'r':
@@ -23,54 +27,46 @@ def test_path(f, x):
     print(y)
     return y
 
-m = 5
-n = 5
+# Setup variables
+m = int(input("# of Moves: "))
+n = int(input("# of Operators: "))
 # o = [op1, op2, op3, op4]
 o = []
-g = 4
-x = 25
-y = None # + 5 / 4 * 3 f + 1
+g = int(input("Goal: "))
+x = int(input("Starting Number: "))
+y = None
 p = []
 l = m - 1
 
 
-current = [0, 0, 0, 0, 0]
+current = []
 
+# Setup current to have # of moves
+for i in range(m):
+    current.append(0)
+
+# Input operators
 for i in range(n):
     operator = input("Operator: ")
     o.append(operator.split())
 
-while l >= 0 and current[l] < n:
-    if current[l] < n:
+# Generate paths
+while current[0] < n:
+    if l == m - 1 and current[l] < n:
         p.append(current.copy())
+        print(current)
         current[l] = current[l] + 1
     else:
+        print(n)
         for i in range(m):
-            if (l + i) < m:
-                current[l + i]
-'''
-while current[0] < n:
-    while current[1] < n:
-        while current[2] < n:
-            while current[3] < n:
-                while current[4] < n:
-                    p.append(current.copy())
-                    current[4] = current[4] + 1
-                current[3] = current[3] + 1
-                current[4] = 0
-            current[2] = current[2] + 1
-            current[3] = 0
-            current[4] = 0
-        current[1] = current[1] + 1
-        current[2] = 0
-        current[3] = 0
-        current[4] = 0
-    current[0] = current[0] + 1
-    current[1] = 0
-    current[2] = 0
-    current[3] = 0
-    current[4] = 0
-'''
+            if current[i + 1] >= n:
+                current[i] = current[i] + 1
+                for j in range(i + 1, m):
+                    current[j] = 0
+                break
+            
+
+
 print(print(len(p), 'paths generated total'))
 
 for i in range(len(p)):
